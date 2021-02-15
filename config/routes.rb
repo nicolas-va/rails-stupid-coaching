7d656914c96c42691feab1f15c18635ba9eaa948
nicolas vassel <nicolas.vassel@gmail.com>
2021-02-15 13:13:46 +0000

    stupid coaching3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  #verbs "/Users/nicolasvassel/code/nicolas-va/rails-stupid-coaching", to: "controller#ask"
+  root to: 'questions#ask'
+  get 'ask', to: 'questions#ask'
+  root to: 'questions#answer'
+  get 'answer', to: 'questions#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
